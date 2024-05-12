8d229172ae7bae21901b026cafbf50818be3c32c
CommieTaco <rolin.azmitia.iii@gmail.com>
2024-05-11 21:59:10 -0600

    First commit

---
+++ src/main/java/com/boombastic/mediateca/MediatecaApplication.java
@@ -0,0 +1,13 @@
+package com.boombastic.mediateca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediatecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediatecaApplication.class, args);
+	}
+
+}
