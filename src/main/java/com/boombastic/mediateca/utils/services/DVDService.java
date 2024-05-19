6ae84b8baf823049c77db9597a0f1e6ef506be0e
CommieTaco <rolin.azmitia.iii@gmail.com>
2024-05-18 22:53:12 -0600

    More Services

--- src/main/java/com/boombastic/mediateca/utils/services/DVDService.java
+++ src/main/java/com/boombastic/mediateca/utils/services/DVDService.java
@@ -1,10 +1,22 @@
 package com.boombastic.mediateca.utils.services;
 
+import com.boombastic.mediateca.utils.dtos.CDDto;
+import com.boombastic.mediateca.utils.dtos.DVDDto;
 import com.boombastic.mediateca.utils.dtos.UsuarioDto;
+import com.boombastic.mediateca.utils.models.CD;
+import com.boombastic.mediateca.utils.models.DVD;
 import com.boombastic.mediateca.utils.models.Usuario;
 
 import java.util.List;
 
 public interface DVDService {
 
+    List<DVDDto> listAllDVDs();
+    DVD saveDVD(DVD dvd);
+
+    DVDDto findDVDById(Long dvdId);
+
+    void updateDVD(DVDDto dvdDto);
+
+    void delete(Long dvdId);
 }
--- src/main/java/com/boombastic/mediateca/utils/services/DocumentoMateriaService.java
+++ src/main/java/com/boombastic/mediateca/utils/services/DocumentoMateriaService.java
@@ -1,10 +1,22 @@
 package com.boombastic.mediateca.utils.services;
 
+import com.boombastic.mediateca.utils.dtos.CDDto;
+import com.boombastic.mediateca.utils.dtos.DocumentoMateriaDto;
 import com.boombastic.mediateca.utils.dtos.UsuarioDto;
+import com.boombastic.mediateca.utils.models.CD;
+import com.boombastic.mediateca.utils.models.DocumentoMateria;
 import com.boombastic.mediateca.utils.models.Usuario;
 
 import java.util.List;
 
 public interface DocumentoMateriaService {
 
+    List<DocumentoMateriaDto> listAllDocumentoMaterias();
+    DocumentoMateria saveDM(DocumentoMateria dm);
+
+    DocumentoMateriaDto findDMById(Long dmId);
+
+    void updateDM(DocumentoMateriaDto dmDto);
+
+    void delete(Long dmId);
 }
