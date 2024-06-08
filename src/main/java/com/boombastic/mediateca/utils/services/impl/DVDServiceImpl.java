4032174dfb1669694c8082da02d9ca8aa2a23e1d
Katherine Beltrán <verme.ouo@gmail.com>
2024-06-08 12:40:48 -0600

    lmpl

--- src/main/java/com/boombastic/mediateca/utils/services/impl/DVDServiceImpl.java
+++ src/main/java/com/boombastic/mediateca/utils/services/impl/DVDServiceImpl.java
@@ -1,42 +1,56 @@
 package com.boombastic.mediateca.utils.services.impl;
 
+
 import com.boombastic.mediateca.utils.dtos.DVDDto;
-import com.boombastic.mediateca.utils.dtos.UsuarioDto;
-import com.boombastic.mediateca.utils.models.Usuario;
-import com.boombastic.mediateca.utils.repository.UsuarioRepository;
-import com.boombastic.mediateca.utils.services.DVDService;
-import com.boombastic.mediateca.utils.services.UsuarioService;
+import com.boombastic.mediateca.utils.models.DVD;
+import com.boombastic.mediateca.utils.services.DvdService;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-public class DVDServiceImpl implements DVDService {
+public class DVDServiceImpl  {
 
+    private List<DVDDto> dvds = new ArrayList<>();
 
     @Override
-    public List<DVDDto> listAllDvds() {
-        return List.of();
+    public List<DVDDto> listAllDvds(){
+        return dvds;
+    }
+    @Override
+    public DVDDto findDvdById(long dvdId){
+        return dvds.stream().filter(dvdDto -> dvdDto.getId().equals(dvdId)).findFirst().orElse(null);
     }
 
     @Override
-    public DVDDto findDvdById(Long dvdId) {
-        return null;
+    public void saveDvd(DVD dvd){
+        DVDDto dvdDto = DVDDto.builder()
+                .id((long)(dvds.size()+1))
+                .idDocumento(dvd.getIdDocumento().getIdDocumento())
+                .director(dvd.getDirector())
+                .genero(dvd.getGenero())
+                .duracion(dvd.getDuracion())
+                .build();
+        dvds.add(dvdDto);
     }
-
     @Override
-    public void updateDvd(DVDDto dvdDto) {
-
+    public void updateDvd(DVDDto dvdDto){
+        DVDDto existingDvd = findDvdById(dvdDto.getId());
+        if(existingDvd != null){
+            existingDvd.setDuracion(dvdDto.getDuracion());
+            existingDvd.setGenero(dvdDto.getGenero());
+            existingDvd.setDirector(dvdDto.getDirector());
+            existingDvd.setIdDocumento(dvdDto.getIdDocumento());
+        }
     }
-
     @Override
-    public void delete(Long dvdId) {
-
+    public void delete(long dvdID){
+        dvds.removeIf(dvdDto -> dvdDto.getId().equals(dvdID));
     }
 
-    @Override
-    public void saveDvd(DVDDto dvd) {
 
-    }
+
+
 }
