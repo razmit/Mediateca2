e03cb5a94c13ac80e85d829272dff988a727d2ac
Carlos0295 <FR230818@ALUMNO.UDB.EDU.SV>
2024-06-07 23:08:08 -0600

    cambios

--- src/main/java/com/boombastic/mediateca/utils/services/impl/CDServiceImpl.java
+++ src/main/java/com/boombastic/mediateca/utils/services/impl/CDServiceImpl.java
@@ -1,16 +1,14 @@
 package com.boombastic.mediateca.utils.services.impl;
 
 import com.boombastic.mediateca.utils.dtos.CDDto;
-import com.boombastic.mediateca.utils.dtos.UsuarioDto;
 import com.boombastic.mediateca.utils.models.CD;
-import com.boombastic.mediateca.utils.models.Usuario;
-import com.boombastic.mediateca.utils.repository.CDRepository;
 import com.boombastic.mediateca.utils.repository.CDRepository;
 import com.boombastic.mediateca.utils.services.CDService;
-import com.boombastic.mediateca.utils.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -18,41 +16,41 @@ public class CDServiceImpl implements CDService {
 
     private CDRepository cdRepository;
 
+    @Autowired
     public CDServiceImpl(CDRepository cdRepository) {
         this.cdRepository = cdRepository;
     }
 
-
     @Override
     public List<CDDto> listAllCDs() {
-
-        List<CD> cds = cdRepository.getAllCDs();
-        return cds.stream().map((cd) -> mapToCDDto(cd)).collect(Collectors.toList());
+        List<CD> cds = cdRepository.findAll();
+        return cds.stream().map(this::mapToCDDto).collect(Collectors.toList());
     }
 
     @Override
     public CD saveCD(CD cd) {
-        return null;
+        return cdRepository.save(cd);
     }
 
     @Override
     public CDDto findCDById(Long cdId) {
-        return null;
+        Optional<CD> cdOptional = cdRepository.findById(cdId);
+        return cdOptional.map(this::mapToCDDto).orElse(null);
     }
 
     @Override
     public void updateCD(CDDto cdDto) {
-
+        CD cd = mapToCD(cdDto);
+        cdRepository.save(cd);
     }
 
     @Override
     public void delete(Long cdId) {
-
+        cdRepository.deleteById(cdId);
     }
 
     public CDDto mapToCDDto(CD cd) {
-
-        CDDto cdDto = CDDto.builder()
+        return CDDto.builder()
                 .id(cd.getIdCdAudio())
                 .documento(cd.getDocumento())
                 .artista(cd.getArtista())
@@ -60,7 +58,16 @@ public class CDServiceImpl implements CDService {
                 .genero(cd.getGenero())
                 .numCanciones(cd.getNumCanciones())
                 .build();
+    }
 
-        return cdDto;
+    public CD mapToCD(CDDto cdDto) {
+        return CD.builder()
+                .idCdAudio(cdDto.getId())
+                .documento(cdDto.getDocumento())
+                .artista(cdDto.getArtista())
+                .duracion(cdDto.getDuracion())
+                .genero(cdDto.getGenero())
+                .numCanciones(cdDto.getNumCanciones())
+                .build();
     }
 }
